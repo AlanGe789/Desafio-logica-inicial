7403d9cffb31970e7cbc648a8f3c7a3976a1e939
Alan <alange102030@gmail.com>
2024-11-10 19:48:55 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let Name= "Vandson"
+let XP= 10
+switch(XP) {
+    case "<=1000":
+        console.log("Ferro")
+            Break
+    case ">=1001 && <=2000":
+        console.log("Bronze")
+            Break
+    case ">=2001 && <=5000":
+        console.log("Prata")
+            Break
+    case ">=6001 && <=7000":
+        console.log("Ouro")
+            Break
+    case ">=7001 && <=8000":
+        console.log("Platina")
+            Break
+    case ">=8001 && <=9000":
+        console.log("Ascendente")
+            Break
+    case ">=9001 && <=10000":
+        console.log("Imortal")
+            Break 
+    case ">=10001":
+        console.log("Radiante")
+            Break
+
+ }
